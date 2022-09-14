90a3ca16992f6f79bd6b08726efca40e32d1dee8
Chris <chris_pens@hotmail.co.uk>
2022-09-14 20:34:08 +0100

    get questions and answers from Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Question } from "./components/question";
+
+const randomInts = (quantity: number, max: number): number[] => {
+  const set = new Set<number>();
+  while (set.size < quantity) {
+    set.add(Math.floor(Math.random() * max));
+  }
+  return [...set];
+};
 
 function App() {
+  const [questions, setQuestions] = useState<
+    { name: string; imageUrl: string }[]
+  >([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentQuestionAnswers, setCurrentQuestionAnswers] = useState<
+    [string, string, string, string] | null
+  >(null);
+  const [allPossibleAnswers, setAllPossibleAnswers] = useState<any>();
+
+  const fetchData = async () => {
+    const response = await fetch("https://api.disneyapi.dev/characters"); // TODO get more than the first 50
+    const data = await response.json();
+
+    const filterData = data.data.filter(
+      (x: any) => x.tvShows.length || x.films.length
+    );
+
+    const tenQuestions = randomInts(10, filterData.length - 1).map((key) => {
+      const { name, imageUrl } = filterData[key];
+      return { name, imageUrl };
+    });
+
+    const fourAnswers = randomInts(4, filterData.length - 1).map((key) => {
+      const { name } = filterData[key];
+      return { name };
+    });
+
+    setAllPossibleAnswers(fourAnswers);
+
+    setQuestions(tenQuestions);
+
+
+
+  console.log(currentQuestionAnswers);
+  console.log(allPossibleAnswers);
+  console.log(currentQuestion);
+  console.log(questions);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {currentQuestionAnswers ? (
+        <Question
+          image={questions[currentQuestion].imageUrl}
+          answers={currentQuestionAnswers}
+          allPossibleAnswers={allPossibleAnswers}
+        />
+      ) : (
+        "Loading...."
+      )}
+      <button onClick={() => setCurrentQuestion(currentQuestion + 1)}>
+        Next
+      </button>
     </div>
   );
 }
